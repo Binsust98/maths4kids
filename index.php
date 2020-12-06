aed63770f4fdb1aca7a1e1b841d6abdccd8f1da9
binsust98 <binsust98@gmail.com>
2020-12-06 14:51:51 +0530

    kid math

---
+++ index.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kid math app</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<link rel="stylesheet" href="css/style.css">
+
+   background color: red;
+<body class="bkgcolor">
+  <nav class="navbar navbar-expand-sm bg-danger navbar-dark">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index1.php">HOME</a>
+          </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="addition.php">ADDITION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="subtraction.php">SUBTRACTION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="multiplication.php">MULTIPLICATION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="division.php">DIVISION</a>
+    </li>
+  </ul>
+</nav>
+
+
+
+          
+
+
+
+     
+
+      
+
+      <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+       <center> <img src="../images/kid1.jpg" height="500" width="1250"  alt="..."></center>    
+          </div>
+          <div class="carousel-item">
+          <center><img src="../images/aaa1.jpg" height="500" width="1250" alt="..."></center>  
+          </div>
+          <div class="carousel-item">
+           <center> <img src="../images\aaaa66.jpg" height="500" width="1250"" alt="..."></center>
+          </div>
+        </div>
+        
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div>
+
+
+    
+        <div class="row">
+
+
+            <div class="col col-12 col-sm-9 col-md-9 col-lg-9 col-xl-9">
+
+              <h2>This website is for you if</h2>
+
+              <p class = "text-justify">
+              
+     
+               
+              
+                You want to help your child learn Math without anxiety, even if you think you’re horrible at it or never liked it at school
+                You want your students to learn the concepts, skills and applications of Math concepts
+                You think Math is more than just doing computations; that it is important to have insights into the reasons behind why things are true!
+             
+              </p>
+
+                 <h2>You CAN teach your child Math....even if you hate it</h2> 
+
+                 <p class = "text-justify">
+
+                  Parents play an important role in the lives of their children. We serve as our child’s first mentors. We teach them how to walk, speak and so, why can’t we teach them elementary math as well? Of course, not all parents out there is fond of the subject math but nevertheless, there is nothing wrong with trying to teach it to your child even if you hate it. After all, if you can overcome your fear of Math, they can, too.
+
+                  We have everything that you would need to teach your child Elementary Math. So, want to be a math teacher to your child at home? You can definitely do it with our help!
+
+
+                </p>
+                
+
+                <div class="row row-cols-1 row-cols-md-2">
+                  <div class="col mb-4">
+                    <div class="card">
+                      <img src="../images/kid02.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">ADDITION</h5>
+                        <p class="card-text">Addition is a mathematical operation. It is a process or action that you do with numbers.
+
+                          The symbol for this process is + (plus). In this process we combine 2 numbers to make a bigger number..</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col mb-4">
+                    <div class="card">
+                      <img src="../images\sub1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">SUBTRACTION</h5>
+                        <p class="card-text">Subtraction is a mathematical operation; it is a process or action that you do with numbers. It is the process of finding the difference of 2 numbers.Remember to use Math terms like subtract, difference, left over, remainder and so on.
+
+                          Examples: "Let's subtract 2 from 9" or "How many do we have left?"
+                          
+                          Do this a few times with different numbers of objects..</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col mb-4">
+                    <div class="card">
+                      <img src="../images\mul1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">MULTIPLICATION</h5>
+                        <p class="card-text">Multiplying means repeated addition of a number. (The number must all be the same before we can use it to multiply.)
+
+                          When you think of it this way, learning the Times Tables makes sense.
+                          
+                          Let's take the 3 times tables, for example.  Students usually learn the 3 times tables this way:
+                          
+                          1 x 3 = 3</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col mb-4">
+                    <div class="card">
+                      <img src="../images/div1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">DIVISION</h5>
+                        <p class="card-text">Division can also be explained this way: We want to divide the total into groups of a certain number and we want to know how many groups there are..</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+
+
+
+
+
+                <div class="col col-12 col-sm-3 col-md-3 col-lg-3 col-xl-3"></div>
+                
+        </div>
+    
+
+
+
+        <div class="card" style="width: 18rem;">
+          <img src="../images\CARD12.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">BASIC MATH</h5>
+            <p class="text-justify" "card-text">There are lots and lots of free, fun Math resources and games for you to use.
+
+              If you are looking beyond textbooks, assessment books and worksheets, then this is the place for you! This site is for anyone who wants a clearer understanding of elementary school Math.
+              
+              Explanations of complex Math concepts are broken down for easy learning.
+              
+              Each elementary Math concept is explained in various ways so your child can understand it in the way that suits him or her.
+              
+             Children have too much written work at school, so I have included lots of learning games for your child to practice what he or she has learnt.You can also use the flashcards and games found here to organize Math clubs with the kids in your neighborhood.
+
+             The best way to teach your child is to go through the topics systematically as they appear in the navigation on the left. Each topic builds the foundation needed for harder concepts.Even if your child is already good in Math, it is advisable to go through all the topics to plug any gaps he or she might have or strengthen his or her understanding.</p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ADDITION</li>
+            <li class="list-group-item">SUBTRACTION</li>
+            <li class="list-group-item">MULTIPLICATION</li>
+            <li class="list-group-item">DIVISION</li>
+          </ul>
+          <div class="card-body">
+            <tr>
+              <td><a href="addition.html" class="addition-link">Addition link</a></td>
+              <td><a href="subtraction.html" class="subtraction-link">Subtraction link</a></td>
+            </tr>
+            <tr>
+             <td><a href="multiplication.html" class="multiplication-link">Multiplication link</a</td>
+             
+              <td><a href="division.html" class="division-link">Division link</a></td>
+            </tr>
+           
+          </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
---
+++ subtraction.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+</head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>subtraction</title>
+</head>
+<link rel="stylesheet" href="css/style.css">
+
+<body class="bgcolr">
+
+    <body>
+    <nav class="navbar navbar-expand-sm bg-danger navbar-dark">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="index1.php">HOME</a>
+          </li>
+      <li class="nav-item ">
+        <a class="nav-link" href="addition.php">ADDITION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="subtraction.php">SUBTRACTION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="multiplication.php">MULTIPLICATION</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="division.php">DIVISION</a>
+    </li>
+  </ul>
+</nav>
+               
+    
+    
+         
+    
+    
+<div class="container">
+
+<div class="row">
+
+<div class="col col-12 col-sm-2"></div>
+<div class="col col-12 col-sm-2"></div>
+
+<table class="table">
+<tr>
+
+    <td>ENTER FIRST NUMBER</td>
+    <td><input type="text" class="form-control" id="num1"></td>
+</tr>
+
+<tr>
+    <td>ENTER SECOND NUMBER</td>
+    <td><input type="text" class="form-control" id="num2"></td>
+</tr>
+
+<tr>
+<td>RESULT</td>
+<td><input type="text" class="form-control" id="res"></td>
+
+</tr>
+
+<tr>
+
+<td></td>
+<td><button onclick="subtraction()" class="btn btn-danger">SUBTRACT</button></td>
+
+</tr>
+
+
+</table>
+
+
+
+
+
+<div class="col col-12 col-sm-2"></div>
+
+</div>
+
+
+
+</div>
+
+<script>
+function subtraction() {
+
+var getnum1=parseInt(document.getElementById("num1").value)
+var getnum2=parseInt(document.getElementById("num2").value)
+
+var result=getnum1-getnum2
+
+document.getElementById("res").value=result
+
+}
+
+
+
+
+
+</script>
+
+
+
+    
+</body>
+</html>
